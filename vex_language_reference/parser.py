a031b82bc04d5823ea8d7a0e7d71a318d3f41848
Jannis <redbeardsescapades@gmail.com>
2025-11-01 15:30:59 +0100

    Initial commit

---
+++ vex_language_reference/parser.py
@@ -0,0 +1,47 @@
+# load input file
+
+# get first word on each row, add to list
+
+# concat words with "|" as separator
+
+# profit
+
+
+
+import os
+import re
+
+
+filepath = "vex_language_reference/functions.txt"
+file =  open(filepath)
+
+functions = []
+
+with open(filepath) as f:
+    for line in file:
+        index = line.find("(")
+        if index != -1:
+            name = line[0:line.find('(')]
+            # print(name)
+            functions.append(name)
+
+functions_joined = []
+
+for name in functions:
+    name = "".join([name, "|"])
+    # print(name)
+    functions_joined.append(name)
+
+# make really long string
+
+full_string = ""
+
+for name in functions_joined:
+    # print(name)
+    full_string = "".join([full_string, name])
+
+
+print(full_string)
+
+with open ('function_names_regex.txt', 'w') as f:
+    f.write(full_string)
